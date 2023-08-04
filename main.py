962bc0fbeb43502abf77572f9f523bb5b33f3742
J <billsmith116@gmail.com>
2023-08-04 21:18:24 +0200

    Changing prefix to '.' to avoid collisions

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ intents.message_content = True
 intents.voice_states = True
 
 def run_bot():
-    bot = commands.Bot(command_prefix='+', intents=intents)
+    bot = commands.Bot(command_prefix='.', intents=intents)
 
     @bot.event
     async def on_ready():
